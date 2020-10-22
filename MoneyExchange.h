c3b0158aebabad540d9392201cde260c884bb757
Tadsawan Waeohong <62160153@go.buu.ac.th>
2020-10-22 17:20:32 +0700

    edit Money Exchange  UI main and Makefile

---
+++ MoneyExchange.h
@@ -0,0 +1,27 @@
+#define MONEYEXCHANGE_H
+#include<iostream>
+#include<fstream>
+#include<sstream>
+#include<cstdlib>
+#include<iomanip>
+#include<string>
+#include<time.h>
+using namespace std;
+class MoneyExchange{
+	private:
+		string CurrencyType;
+		float MoneyAmount;
+		string PaymentType;
+		float PaymentMoney;
+		string Account;
+	public:
+		MoneyExchange();
+		void SetMoneyExchange(int c,float m,int p);
+		void ExchangeCalculate(int c);
+		string TimeBill();
+		void ShowBillCash(float cash);
+		void ShowPaymentMoney();
+		void SetAccount(string account);
+		void ShowBillAccount();
+		void WriteFileCash();
+};
---
+++ UI.h
@@ -0,0 +1,26 @@
+#ifndef UI_H
+#define UI_H
+#include <iostream>
+#include <sstream>
+#include "Information.h"
+using namespace std;
+class UI{
+    private:
+        string username,password,accountNumber,name,citizenID,birthDate,phoneNumber,typeAccount;
+        int money;
+    public:
+        UI();
+        ~UI();
+        void print_MainMenu();
+        void print_Login();
+        //It has Funtion Check Username and Password
+        void print_menuClerk();
+        void print_menuCustomer();
+        void printmenuRegister_customer();
+        void printmenuRegister_bankClerk();
+        void printInfoFromFileRegister();
+        void MenageRegister(int Number);
+        void print_CurrencyType();
+        void print_PaymentType();
+};
+#endif
