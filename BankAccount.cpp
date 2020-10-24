c153768dd739d5dd9ab2c85daaf9e4abe88f5492
BobbieSurapitchaya <72857966+BobbieSurapitchaya@users.noreply.github.com>
2020-10-24 17:15:36 +0700

    เพิ่มการเขียนลง file และ statement

--- BankAccount.cpp
+++ BankAccount.cpp
@@ -1,4 +1,35 @@
 #include "BankAccount.h"
+
+BankAccount :: statementBill :: statementBill(string name,string accountNumber,string money,string date,string time){
+    Name = name;
+    AccountNumber = accountNumber;
+    Money = money;
+    Date = date;
+    Time = time;
+    linkBill = NULL;
+}
+BankAccount :: BankAccount(){
+    this->headBill = NULL;
+    this->tailBill = NULL;
+    this->tempBill = NULL;
+    this->Obj_BankAccount = NULL;
+    this->temp = NULL;
+    NameAccount = " ";
+    accountNumber = " ";
+    moneyAccount = 0;
+}
+void BankAccount :: AddStatementBill(string name,string accountNumber,string money,string date,string time){
+     statementBill *bill= new statementBill(name,accountNumber,money,date,time);
+            if(headBill == NULL){
+                headBill = bill;
+                tailBill = bill;
+            }
+            else{
+                tailBill->linkBill = bill;
+                tailBill = bill;
+            }
+            Count_Customer++;
+}
 bool BankAccount ::CheckTransfer_account(string accountNumber){
     string name;
 	for(temp = HeadInfo_BankAccount;temp != NULL ; temp = temp->link){
@@ -9,14 +40,20 @@ bool BankAccount ::CheckTransfer_account(string accountNumber){
 	}
 	return false;
 }
-string BankAccount :: getMoneyAccount(string username){
+double BankAccount :: getMoneyAccount(string username){
     string money;
-   for(temp = HeadInfo_BankAccount;temp != NULL ; temp = temp->link){
+    stringstream ss;
+    for(temp = HeadInfo_BankAccount;temp != NULL ; temp = temp->link){
 	   if(username==temp->Username){
+            NameAccount = temp->Name;
+            accountNumber = temp->AccountNumber;
             money = temp->Money;
        }
     } 
-	return money;
+    ss << money ;
+	ss >> moneyAccount;
+	ss.clear();
+	return moneyAccount;
 }
 string BankAccount :: setDateandTime(){
     string Date,Time;
@@ -31,64 +68,96 @@ string BankAccount :: setDateandTime(){
     Time = time;
     return Date+","+Time;
 }
-void BankAccount :: payBill(string group, string username,double moneyAccount, double amount){
+void BankAccount :: payBill(string group, string username, double amount){
     double balance=0,fee=0;
-    string TypeBill, Balance, date, time;
+    string TypeBill, Balance,Amount, date, time;
 
-    if(group=="1"){
-        TypeBill = "Water bill";
-        moneyAccount = moneyAccount - amount;
-        fee = amount*0.3;
-        balance = moneyAccount - fee;
-    }
-    else if(group=="2"){
-        TypeBill = "Electricity bill";
-        moneyAccount = moneyAccount - amount;
-        fee = amount*0.4;
-        balance = moneyAccount - fee;
-    }
-    else if(group=="3"){
-        TypeBill = "Phone bill";
-        moneyAccount = moneyAccount - amount;
-        fee = amount*0.5;
-        balance = moneyAccount - fee;
-    }
-	cout << "*********Bill********" << endl;
     //setTime
-	date=setDateandTime();
+    date=setDateandTime();
     time=date.substr((date.find(",")+1),date.find(" "));
     date=date.substr(0,date.find(","));
 
-    cout << "TypeBill: " << TypeBill << endl;
-    cout << "Fee: " << fee << " baht"<< endl;
-    cout << "Balance: " <<  balance << " baht" << endl;
-    cout << "Date: " << date << " : " << time << endl;
-    
-    //ChangData at File Account
-    stringstream ss; 
-    ss << balance;
-	ss >> Balance;
-	ss.clear();
-	for(temp = HeadInfo_BankAccount;temp !=NULL ; temp = temp->link){
-       if(username==temp->Username){
-            temp->Money = Balance;
-            cout << "Yes" << endl;
-            
-       }
+    if(username.length()==2){//Customer
+        if(group=="1"){
+            TypeBill = "Water bill";
+            moneyAccount = moneyAccount - amount;
+            fee = amount*0.3;
+            balance = moneyAccount - fee;
+        }
+        else if(group=="2"){
+            TypeBill = "Electricity bill";
+            moneyAccount = moneyAccount - amount;
+            fee = amount*0.4;
+            balance = moneyAccount - fee;
+        }
+        else if(group=="3"){
+            TypeBill = "Phone bill";
+            moneyAccount = moneyAccount - amount;
+            fee = amount*0.5;
+            balance = moneyAccount - fee;
+        }
+        cout << "*********Bill********" << endl;
+        cout << "TypeBill: " << TypeBill << endl;
+        cout << "Fee: " << fee << " baht"<< endl;
+        cout << "Balance: " <<  balance << " baht" << endl;
+        cout << "Date: " << date << " : " << time << endl;
+        
+        //ChangData at File Account
+        stringstream ss; 
+        ss << balance;
+        ss >> Balance;
+        ss.clear();
+        ss << amount;
+        ss >> Amount;
+        ss.clear();
+        for(temp = HeadInfo_BankAccount;temp !=NULL ; temp = temp->link){
+            if(username==temp->Username){
+                    temp->Money = Balance;
+                    cout << "Yes" << endl;
+            }
+        }
+        //username,accountNumber,amount,date,time;
+        cout << "*******Chengce******" << endl;
+        ShowBankAccount();//test
+        SaveInfoCustomerToFile();//saveFile
+        /*string fileName;
+        fstream myfile;
+        fileName = "BankAccount.dat";
+        myfile.open(fileName.c_str(),std::ios::out);
+
+        temp=HeadInfo_BankAccount;
+        while(temp!=NULL){
+            myfile << temp->Name << "," << temp->AccountNumber << "," << temp->Money << "," << temp->Username << "," << temp->Password << endl;
+            temp = temp->link;
+        }
+        myfile.close();*/
+
+        AddStatementBill(NameAccount,accountNumber,Amount,date,time);
+        WriteStatement();
     }
-
-	cout << "*******Chengce******" << endl;
-	ShowBankAccount();//test
-    chengceDataFile();
+    /*else{//Bank clerk
+        if(group=="1"){
+            amount*3;
+        }
+        else if(group=="2"){
+            amount*4;
+        }
+        else if(group=="3"){
+            amount*5
+        }
+        
+    }*/
 }
-void BankAccount :: chengceDataFile(){
-    string fileName;
+void BankAccount :: WriteStatement(){
+    string fac,num,fileName,data;
     fstream myfile;
-    fileName = "BankAccount.dat";
-    myfile.open(fileName.c_str(),std::ios::out);
+    fileName = "Statement.dat";
+    myfile.open(fileName.c_str(),std::ios::app);
 
-    for(temp = HeadInfo_BankAccount;temp !=NULL ; temp = temp->link){
-        myfile << temp->Name << "," << temp->AccountNumber << "," << temp->Money << "," << temp->Username << "," << temp->Password << endl;
+    tempBill=headBill;
+    while(tempBill!=NULL){
+        myfile << tempBill->Name << "," << tempBill->AccountNumber << "," << tempBill->Money << "," << tempBill->Date << "," << tempBill->Time << endl;
+		tempBill = tempBill->linkBill;
 	}
 	myfile.close();
 }
--- BankAccount.h
+++ BankAccount.h
@@ -10,14 +10,27 @@
 using namespace std;
 class BankAccount : public Information{
     private:
+        class statementBill{
+            public:
+                string Name,AccountNumber,Money,Date,Time;
+                statementBill *linkBill;
+                statementBill(string name,string accountNumber,string money,string date,string time);
+                //Call Funtion Info From class Person
+        };
+        statementBill *headBill, *tailBill, *tempBill;
         BankAccount *Obj_BankAccount;
         Info_BankAccount *temp;
+        string accountNumber, NameAccount;
+        double moneyAccount;
+        int countStatment;
     public:
+        BankAccount();
+        void AddStatementBill(string name,string accountNumber,string money,string date,string time);
         bool CheckTransfer_account(string accountNumber);
-        string getMoneyAccount(string username);
+        double getMoneyAccount(string username);
         string setDateandTime();
-        void payBill(string group, string username, double moneyAccount, double amount);
-        void chengceDataFile();
+        void payBill(string group, string username, double amount);
+        void WriteStatement();
     
 };
 #endif
